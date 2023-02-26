429456f7e9b72a40e9efc9d99e8230f5d7a30225
Davenb42 <david.benavides.n@gmail.com>
2023-02-26 12:45:20 -0600

    PS-33 #comment post viedogame to database

--- Backend/server/src/controllers/admin-videogames.controller.js
+++ Backend/server/src/controllers/admin-videogames.controller.js
@@ -1,67 +1,15 @@
-const express = require('express')
-const { Router } = require("express");
-const bodyParser = require('body-parser');
-const urlencodedParser = bodyParser.urlencoded({ extended: false })
-const jsonParser = bodyParser.json()
-const router = Router();
-const multer = require("multer")
-const {subirArchivo, obtenerArchivo} = require('./s3')
-
 const adminVideogameController = {};
 
 const Videojuego = require("../models/videogame");
-
-//storage
-const storage = multer.diskStorage({
-  destination: "client/public/assets/images",
-  filename: (req, file, callback, filename) => {
-    var ext = path.extname(file.originalname);
-    try {
-      if(ext !== '.png' && ext !== '.jpg' && ext !== '.gif' && ext !== '.jpeg' ) {
-        throw new Error(" no es una imagen \n")
-      }
-      callback(null, uuidv4() + ext);
-    } catch (error) {
-      console.log("El archivo selecionado" + error.name + "Solo se admiten archivos tipo: \n .png \n .jpg \n .gif \n .jpeg")
-    }
-  }
-}) 
-
-var subida = multer({storage})
-router.use(express.static(__dirname));
-router.use(jsonParser);
-
-router.get('/imagen/:key', (req, res) => {
-  const key = req.params.key;
-  const readStream = obtenerArchivo(key);
-  readStream.pipe(res);
-})
+const fs = require('fs')
 
 //Funciones
-
-adminVideogameController.subirVideojuego = subida.single('imagen'), async function (req, res) {
-  try{
-      const result = await subirArchivo(req.file);
-      const videojuego = new Videojuego({
-          id: Number(req.body.id),
-          titulo: req.body.titulo,
-          portada: result.Key,
-          sinopsis: req.body.sinopsis,
-          usuario: req.body.usuario,
-          juegoZip: result.Key,
-      })
-      await videojuego.save();
-      res.send("Videogame posted successfully");
-  } catch (err){
-      res.send(err);
-  }
-}
-
-
-adminVideogameController.postVideogame = async (req, res) => {
-  const newVideogame = new Videojuego(req.body);
-  await newVideogame.save();
+adminVideogameController.postVideogame = async function (req, res) {
+  req.body.portada.data = fs.readFileSync(req.body.imagepath)
+  req.body.juegoZip.data = fs.readFileSync(req.body.filepath)
+  const videojuego = new Videojuego(req.body)
+  await videojuego.save();
   res.send("Videogame posted successfully");
-};
+}
 
 module.exports = adminVideogameController;
--- Backend/server/src/routes/admin-videogames.routes.js
+++ Backend/server/src/routes/admin-videogames.routes.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
-const router = Router();
+const upload = require("../controllers/admin-videogames.controller");
 
+const router = Router();
 const adminVideogameController = require("../controllers/admin-videogames.controller");
 const token = require("../controllers/token");
 
@@ -8,9 +9,8 @@ const token = require("../controllers/token");
 * Cuando se inserte el token
 * --------------------------
 * router.post("/", token.verifyToken, adminVideogameController.postVideogame);
-*/
+*/ 
 
 router.post("/post", adminVideogameController.postVideogame);
-router.post("/subirVideojuego", adminVideogameController.subirVideojuego);
 
 module.exports = router;
