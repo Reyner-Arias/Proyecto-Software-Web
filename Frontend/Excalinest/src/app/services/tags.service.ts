e3ba553eea97a72d4930ff801c5890e9de8d00d9
Kenneth Ibarra Vargas <kiv19022000@gmail.com>
2023-03-11 19:53:51 -0600

    PS-39 #comment Tags Service to post a tag in Angular

---
+++ Frontend/Excalinest/src/app/services/tags.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TagsService {
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+  adminPostAPI = 'http://localhost:3000/admin-tags/post'
+
+  postTag (tag: any) {
+    return this.http.post(this.adminPostAPI, tag, {responseType: 'text'});
+  }
+
+}
