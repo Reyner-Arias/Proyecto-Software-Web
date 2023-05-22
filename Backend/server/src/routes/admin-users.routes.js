7d11b41e8834826e84da29c54588febe4b0fdd2a
Reyner-Arias <Rey86581@gmail.com>
2023-05-21 22:05:55 -0600

    Testing github actions

--- Backend/server/src/routes/admin-users.routes.js
+++ Backend/server/src/routes/admin-users.routes.js
@@ -12,7 +12,7 @@ router.get('/get-all', adminUserController.getAllUsers);
 // Editar un usuario
 router.put('/put/:_id', adminUserController.putUser);
 
-// Eliminar un usuario por email
+// Eliminar un usuario
 router.delete('/delete/:email', adminUserController.deleteUser);
 
 module.exports = router
