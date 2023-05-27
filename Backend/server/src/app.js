68f45ffe0cd68a0a815a5a8ea1b83fd7398faa17
Reyner-Arias <Rey86581@gmail.com>
2023-05-26 20:55:07 -0600

    Adding security headers

--- Backend/server/src/app.js
+++ Backend/server/src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 const cors = require("cors");
+const helmet = require('helmet');
 
 const port = process.env.PORT || 3000;
 
@@ -12,6 +13,7 @@ app.use(morgan("dev"));
 app.use(express.json())
 app.use(express.urlencoded({extended: false }))
 
+app.use(helmet());
 app.use(cors())
 /* Enrutadores */
 app.use("/admin-videogames", require("./routes/admin-videogames.routes"));
